bc912fc9bcf75062fb7e4ac9ee5155c01d43b5fa
Andrew Elkins <Andrew.j.elkins@gmail.com>
2022-04-14 01:41:03 -0400

    min

--- dashboard.js
+++ dashboard.js
@@ -22,17 +22,41 @@ async function transferNative(){
   const amount = document.getElementById('native-amount').value;
 
   // sending 0.5 ETH
-  const option = {
+  const options = {
     type: "native", 
     amount: Moralis.Units.ETH(amount), 
-    receiver: address}
+    receiver: address
+  }
   let result = await Moralis.transfer(options)
 }
+
+async function transferERC20(){
+  const address = document.getElementById('erc20-address').value;
+  const amount = document.getElementById('erc20-amount').value;
+  const contract = document.getElementById('erc20-contract').value;
+  const decimals = document.getElementById('erc20-decimals').value;
+
+  // sending 0.5 ETH
+  const options = {
+    type: "erc20", 
+    amount: Moralis.Units.Token(amount, decimals), 
+    receiver: address,
+    contractAddress: contract
+  }
+  let result = await Moralis.transfer(options)
+}
+
+//CHAINLINK RINKEBY ADDRESS
+//0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+//Decimals: 18
 
 //Executes functions defined above on a click with the id tag hookup
 document.getElementById("btn-login").onclick = login;
 document.getElementById("btn-logout").onclick = logout;
+
+//FUNCTION CLICK LISTENERS
 document.getElementById("transfer-native").onclick = transferNative;
+document.getElementById("transfer-erc20").onclick = transferERC20;
 
 //OLD BOOTSTRAP CODE
 (function () {
